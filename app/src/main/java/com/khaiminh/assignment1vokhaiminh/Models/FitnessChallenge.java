64e49ccafb712f50b7b2c1d1e2db87a3369805ae
minh0811 <vokhaiminh0811@gmail.com>
2023-11-16 10:26:12 +0700

    Add image in res/drawable and add a image place in the model and change the layout of the recyler

--- app/src/main/java/com/khaiminh/assignment1vokhaiminh/Models/FitnessChallenge.java
+++ app/src/main/java/com/khaiminh/assignment1vokhaiminh/Models/FitnessChallenge.java
@@ -6,14 +6,16 @@ public class FitnessChallenge {
     private String description;
     private int duration; // in days
     private String challengeType;
+    private int imageResId;
 
     // Constructor
-    public FitnessChallenge(int id, String name, String description, int duration, String challengeType) {
+    public FitnessChallenge(int id, String name, String description, int duration, String challengeType, int imageResId) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.duration = duration;
         this.challengeType = challengeType;
+        this.imageResId = imageResId;
     }
 
     // Getters and setters
@@ -32,6 +34,10 @@ public class FitnessChallenge {
     public String getChallengeType() {
         return challengeType;
     }
+    public int getImageResId() {
+        return imageResId;
+    }
+
 
     public void setId(int id) {
         this.id = id;
@@ -47,5 +53,8 @@ public class FitnessChallenge {
     }
     public void setChallengeType(String challengeType) {
         this.challengeType = challengeType;
+    }
+    public void setImageResId(int imageResId) {
+        this.imageResId = imageResId;
     }
 }
