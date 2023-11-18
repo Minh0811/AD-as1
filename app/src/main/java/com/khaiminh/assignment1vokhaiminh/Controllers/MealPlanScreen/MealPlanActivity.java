b768c0cc68c130b8aa258e6c2095dd049602b9d5
minh0811 <vokhaiminh0811@gmail.com>
2023-11-18 15:50:09 +0700

    Add meal plan activity

---
+++ app/src/main/java/com/khaiminh/assignment1vokhaiminh/Controllers/MealPlanScreen/MealPlanActivity.java
@@ -0,0 +1,16 @@
+package com.khaiminh.assignment1vokhaiminh.Controllers.MealPlanScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.khaiminh.assignment1vokhaiminh.R;
+
+public class MealPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_plan);
+    }
+}
