11140578e105b0c40c359d326fc07f6376b3c37d
minh0811 <vokhaiminh0811@gmail.com>
2023-11-18 17:59:04 +0700

    Finished meal plan activity

--- app/src/main/java/com/khaiminh/assignment1vokhaiminh/Controllers/MealPlanScreen/MealPlanActivity.java
+++ app/src/main/java/com/khaiminh/assignment1vokhaiminh/Controllers/MealPlanScreen/MealPlanActivity.java
@@ -1,20 +1,22 @@
 package com.khaiminh.assignment1vokhaiminh.Controllers.MealPlanScreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-
-import com.khaiminh.assignment1vokhaiminh.R;
-
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-
-// Inside MealPlanActivity.java
-
 import android.widget.EditText;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.khaiminh.assignment1vokhaiminh.Models.MealPlan;
+import com.khaiminh.assignment1vokhaiminh.R;
+import com.khaiminh.assignment1vokhaiminh.Utils.JsonUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MealPlanActivity extends AppCompatActivity {
 
@@ -22,11 +24,22 @@ public class MealPlanActivity extends AppCompatActivity {
     private EditText editTextHeight;
     private Button buttonCalculate;
     private TextView textViewBmiResult;
+    private RecyclerView recyclerViewMealPlans;
+    private MealPlanAdapter mealPlanAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_plan);
+
+        ImageButton btnBackToExercise = findViewById(R.id.btnBackToExercise);
+        btnBackToExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the current activity
+                finish();
+            }
+        });
 
         editTextWeight = findViewById(R.id.editTextWeight);
         editTextHeight = findViewById(R.id.editTextHeight);
@@ -39,6 +52,9 @@ public class MealPlanActivity extends AppCompatActivity {
                 calculateBMI();
             }
         });
+
+        recyclerViewMealPlans = findViewById(R.id.recyclerViewMealPlans);
+        recyclerViewMealPlans.setLayoutManager(new LinearLayoutManager(this));
     }
 
     private void calculateBMI() {
@@ -51,7 +67,20 @@ public class MealPlanActivity extends AppCompatActivity {
 
             float bmi = weight / (height * height);
             textViewBmiResult.setText("Your BMI: " + bmi);
-            // Here you can add logic to recommend a meal plan based on the BMI
+
+            // Load and filter meal plans
+            displayMealPlansForBMI(bmi);
         }
+    }
+
+    private void displayMealPlansForBMI(float bmi) {
+        String json = JsonUtils.loadJSONFromAsset(this, "meal_plans.json");
+        List<MealPlan> mealPlans = JsonUtils.parseMealPlans(json);
+        List<MealPlan> filteredMealPlans = mealPlans.stream()
+                .filter(mealPlan -> bmi >= mealPlan.getMinBmi() && bmi <= mealPlan.getMaxBmi())
+                .collect(Collectors.toList());
+
+        mealPlanAdapter = new MealPlanAdapter(filteredMealPlans);
+        recyclerViewMealPlans.setAdapter(mealPlanAdapter);
     }
 }
---
+++ app/src/main/java/com/khaiminh/assignment1vokhaiminh/Controllers/MealPlanScreen/MealPlanAdapter.java
@@ -0,0 +1,53 @@
+package com.khaiminh.assignment1vokhaiminh.Controllers.MealPlanScreen;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.khaiminh.assignment1vokhaiminh.Models.MealPlan;
+import com.khaiminh.assignment1vokhaiminh.R;
+import java.util.List;
+
+public class MealPlanAdapter extends RecyclerView.Adapter<MealPlanAdapter.MealPlanViewHolder> {
+
+    private List<MealPlan> mealPlanList;
+
+    public MealPlanAdapter(List<MealPlan> mealPlanList) {
+        this.mealPlanList = mealPlanList;
+    }
+
+    @NonNull
+    @Override
+    public MealPlanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.meal_plan_item, parent, false);
+        return new MealPlanViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MealPlanViewHolder holder, int position) {
+        MealPlan mealPlan = mealPlanList.get(position);
+        holder.textViewMealPlanName.setText(mealPlan.getName());
+        holder.textViewMealPlanDescription.setText(mealPlan.getDescription());
+        // Set other fields as needed
+    }
+
+    @Override
+    public int getItemCount() {
+        return mealPlanList.size();
+    }
+
+    static class MealPlanViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewMealPlanName;
+        TextView textViewMealPlanDescription;
+        // Other views
+
+        public MealPlanViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewMealPlanName = itemView.findViewById(R.id.textViewMealPlanName);
+            textViewMealPlanDescription = itemView.findViewById(R.id.textViewMealPlanDescription);
+            // Initialize other views
+        }
+    }
+}
